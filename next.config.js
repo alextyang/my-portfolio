0da6a8b585ca794a5ab150aeb92935d5d1b44f62
Alex Yang <koopakondra@gmail.com>
2024-02-26 18:00:29 -0500

    🔗 Adjusted for custom domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/my-portfolio",
+    // basePath: "/my-portfolio", // <=== github pages domain requirement
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: {
