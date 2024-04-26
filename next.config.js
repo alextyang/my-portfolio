8ca24bc2c53c1684e0761c6b2b91a6d87fb76317
alextyang <koopakondra@gmail.com>
2024-04-26 12:08:17 -0400

    😇 Added redirect tracker

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: '/portfolio',
-                destination: '/',
+                destination: '/?portfolio',
                 permanent: true,
             },
         ]
